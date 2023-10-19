b7cfaf8b56fc3f043a54dc2eee4a747f1443f745
deagus <dedeaskyt121@gmail.com>
2023-10-19 23:24:53 +0800

    up all features

--- app/src/main/java/com/example/rock_paper_scissors/MainActivity.kt
+++ app/src/main/java/com/example/rock_paper_scissors/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.rock_paper_scissors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var computerImageView: ImageView
+    private lateinit var rockImageButton: ImageButton
+    private lateinit var scissorsImageButton: ImageButton
+    private lateinit var paperImageButton: ImageButton
+    private lateinit var resultImageView: ImageView
+
+    private fun initComponents() {
+        computerImageView = findViewById(R.id.computerImageView)
+        rockImageButton = findViewById(R.id.rockImageButton)
+        scissorsImageButton = findViewById(R.id.scissorsImageButton)
+        paperImageButton = findViewById(R.id.paperImageButton)
+        resultImageView = findViewById(R.id.resultImageView)
+    }
+
+    private fun initListeners() {
+        rockImageButton.setOnClickListener { startGame("ROCK") }
+        scissorsImageButton.setOnClickListener { startGame("SCISSORS") }
+        paperImageButton.setOnClickListener { startGame("PAPER") }
+    }
+
+    private fun startGame(option: String) {
+        val computerOption = Game.pickRandomOption()
+        computerImageView.setImageResource(Game.pickDrawable(computerOption))
+
+        when {
+            Game.isDraw(option, computerOption) -> resultImageView.setImageResource(R.drawable.draw)
+            Game.isWin(option, computerOption) -> resultImageView.setImageResource(R.drawable.win)
+            else -> resultImageView.setImageResource(R.drawable.lose)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.game)
+
+        initComponents()
+        initListeners()
     }
+
 }
